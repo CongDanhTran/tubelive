1c40e2e04526109b10626cfc9c3612e51c4896c3
cex-01-26 <desktop>
2026-04-03 02:54:18 +0100

    test local notifcation

--- sw.js
+++ sw.js
@@ -136,3 +136,15 @@ self.addEventListener('offline', () => {
 //     event.waitUntil(self.registration.update());
 //   }
 // });
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then(clientList => {
+      for (const client of clientList) {
+        if (client.url === '/' && 'focus' in client) return client.focus();
+      }
+      if (clients.openWindow) return clients.openWindow('/');
+    })
+  );
+});
